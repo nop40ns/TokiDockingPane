1e86c476cca0499a915d7e9a6e5aa1b35c5ed731
nop40ns <nop40ns@gmail.com>
2026-08-21 14:16:23 +0900

    動くようになった

---
+++ TokiDockingPane/Models/TokiDragDropPayload.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokiDockingPane.Interfaces;
+
+namespace TokiDockingPane.Models
+{
+    public class TokiDragDropPayload
+    {
+        /// <summary>
+        /// ドラッグ元のノード（画面分割の起点）
+        /// </summary>
+        public IPaneNode SourceNode { get; set; }
+
+        /// <summary>
+        /// ドラッグされているタブ（ViewModel）のデータ実体
+        /// </summary>
+        public TabViewModel DraggedData { get; set; }
+
+        public TokiDragDropPayload(IPaneNode sourceNode, TabViewModel draggedData)
+        {
+            SourceNode = sourceNode;
+            DraggedData = draggedData;
+        }
+    }
+}
